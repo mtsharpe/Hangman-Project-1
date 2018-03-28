f358c2f2b8e5c1cbe44ebd31d06eabb3bda4f46a
“Matthew <mattsharpe72@gmail.com>
2018-03-28 17:47:28 -0400

    more game logic

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let wordLetters = secretWord.split('')
 
 let inputs = document.querySelectorAll('.buttons')
 let guessNumber = document.querySelector('.countdown')
+let resetButton = document.querySelector('.reset')
 
 let matches = 0
 let misses = 0
@@ -15,105 +16,71 @@ function createWord () {
     letters.classList.add('letters')
     document.querySelector('.word').appendChild(letters)
     letters.innerHTML = wordLetters[i]
+    letters.classList.add(wordLetters[i], 'blank')
   }
   console.log(wordLetters)
 }
 createWord()
 
-document.querySelectorAll('.letters').forEach(function (letter) {
-  letter.classList.add('blank')
-})
+// document.querySelectorAll('.letters').forEach(function (letter) {
+//   letter.classList.add('blank')
+// })
 
 function checkLetters () {
   inputs.forEach(function (input) {
     input.addEventListener('click', function () {
       let inputGuess = input.value
       input.setAttribute('class', 'clicked')
-      wordLetters.forEach(function (letter) {
-        if (inputGuess === letter) {
-          document.querySelectorAll('.letters').forEach(function (letter) {
-            letter.classList.remove('.blank')
-          })
-          matches++
-          checkWin()
-          console.log(letter, matches)
-        }
-        else {
-          hangman()
-        }
-      })
+      if (wordLetters.indexOf(inputGuess) >= 0) {
+        document.innerHTML.remove('.blank')
+        matches++
+        checkWin()
+      } else {
+        misses++
+        hangman()
+      }
     })
   })
 }
+
 checkLetters()
 
 function checkWin () {
   if (matches === wordLetters.length) {
-    document.querySelector('.guesses').innerHTML = 'WINNER!!! <br> Would you like to play again?'
-    reset()
+    document.querySelector('.word').innerHTML = '<h4>WINNER!!!<h4>'
   }
 }
+
 function hangman () {
-  switch (misses) {
-    case 1:
-      document.querySelector('.noose').classList.add('hung')
-      guessNumber.innerHTML -= 1
-      break
-    case 2:
-      document.querySelector('.head').classList.add('headhung')
-      guessNumber.innerHTML -= 1
-      break
-    case 3:
-      document.querySelector('.torso').classList.add('hung')
-      guessNumber.innerHTML -= 1
-      break
-    case 4:
-      document.querySelector('.leftArm').classList.add('hung')
-      guessNumber.innerHTML -= 1
-      break
-    case 5:
-      document.querySelector('.rightArm').classList.add('hung')
-      guessNumber.innerHTML -= 1
-      break
-    case 6:
-      document.querySelector('.leftLeg').classList.add('hung')
-      guessNumber.innerHTML -= 1
-      break
-    case 7:
-      document.querySelector('.rightLeg').classList.add('hung')
-      document.querySelector('.guesses').innerHTML = 'YOU LOSE! <br> Would you like to play again?'
-   // reset()
+  if (misses === 1) {
+    document.querySelector('.noose').classList.add('hung')
+    guessNumber.innerHTML--
+  } else if (misses === 2) {
+    document.querySelector('.head').classList.add('headhung')
+    guessNumber.innerHTML--
+  } else if (misses === 3) {
+    document.querySelector('.torso').classList.add('hung')
+    guessNumber.innerHTML--
+  } else if (misses === 4) {
+    document.querySelector('.leftArm').classList.add('hung')
+    guessNumber.innerHTML--
+  } else if (misses === 5) {
+    document.querySelector('.rightArm').classList.add('hung')
+    guessNumber.innerHTML--
+  } else if (misses === 6) {
+    document.querySelector('.leftLeg').classList.add('hung')
+    guessNumber.innerHTML--
+  } else if (misses === 7) {
+    document.querySelector('.rightLeg').classList.add('hung')
+    document.querySelector('.word').innerHTML = '<h4>YOU LOSE!!!<h4>' 
   }
 }
 
-//   if (misses === 1) {
-//     document.querySelector('.noose').classList.add('hung')
-//     guessNumber.innerHTML -= 1
-//   } else if (misses === 2) {
-//     document.querySelector('.head').classList.add('headhung')
-//     guessNumber.innerHTML -= 1
-//   } else if (misses === 3) {
-//     document.querySelector('.torso').classList.add('hung')
-//     guessNumber.innerHTML -= 1
-//   } else if (misses === 4) {
-//     document.querySelector('.leftArm').classList.add('hung')
-//     guessNumber.innerHTML -= 1
-//   } else if (misses === 5) {
-//     document.querySelector('.rightArm').classList.add('hung')
-//     guessNumber.innerHTML -= 1
-//   } else if (misses === 6) {
-//     document.querySelector('.leftLeg').classList.add('hung')
-//     guessNumber.innerHTML -= 1
-//   } else if (misses === 7) {
-//     document.querySelector('.rightLeg').classList.add('hung')
-//     document.querySelector('.guesses').innerHTML = 'YOU LOSE! <br> Would you like to play again?'
-//    // reset()
-//   }
-// }
+function reset () {
+  document.querySelectorAll('.man').classList.remove('hung')
+  document.querySelector('.head').classList.remove('headhung')
+  document.querySeletor('.guesses').innerHMTL = 'You have <span class="".countdown">7</span> guesses remaining</h2>'
+  createWord()
+}
 
-// // function reset () {
-// //   document.querySelectorAll('.man').forEach(function () {
-
-// //   })
-// //   createWord()
-// // }
+document.querySelector('.reset').addEventListener('click', resetButton)
